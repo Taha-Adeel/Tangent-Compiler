e5f7cdabcee93ea43d8867a6ae0742b4bd9c6328
CS20BTECH11004 <cs20btech11004@iith.ac.in>
2022-09-04 15:42:49 +0530

    added token.h, removed src folder

---
+++ lexer/token.h
@@ -0,0 +1,28 @@
+/*
+contains the tokens to be used in lexel.l
+
+aman:
+    Types of Tokens:
+    Keywords, Identifiers, literals(numeric, boolean and pointer), literals (strings), user-defined literals, operators, punctuators.
+
+    Keywords:
+    tokens,33 separate  find in the language specification
+
+taha:
+    Identifiers:
+    One grammar rule, one token
+
+    Numeric, boolean and pointer literals:
+    One grammar rule, one token
+
+    String literal:
+    One grammar rule, one token
+
+    User defined literal:
+    ??rules, one token
+
+    Operators:
+    17 rules and tokens
+
+    Punctuators:
+*/
